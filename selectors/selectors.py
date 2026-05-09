00d5b1c53fb781e44dcef12c004c866ef6564f84
Kristiyan Radev <kradev@evolution.com>
2026-05-09 16:47:07 +0300

    feat: add selectors module with all page selectors

---
+++ selectors/selectors.py
@@ -0,0 +1,51 @@
+class LoginSelectors:
+    EMAIL_INPUT     = "input[autocomplete='email']"
+    PASSWORD_INPUT  = "input[type='password']"
+    SUBMIT_BUTTON   = "button[type='submit']"
+
+
+class NavSelectors:
+    # Navigating directly via URL - the span is display:none
+    PARTNERS_PATH   = "/partners"
+
+
+class PartnersPageSelectors:
+    NEW_PARTNER_BUTTON  = "button:has-text('New partner')"
+    SEARCH_INPUT        = "#search-partners"
+    TABLE_ROW           = "tr.ant-table-row"
+    PARTNER_NAME_CELL   = "span.Cq6YF"
+    ACTION_BUTTON       = "#action-button"
+
+
+class PartnerFormSelectors:
+    # Text inputs - all have real IDs, CSS is clean
+    NAME_INPUT          = "#name-field"
+    ADDRESS_INPUT       = "#address-field"
+    PHONE_INPUT         = "#phone-field"
+    CONTACT_INPUT       = "#contact-person-field"
+    DESCRIPTION_INPUT   = "#description-field"
+
+    # Ant Design Select dropdowns - click the input to open
+    TYPE_DROPDOWN       = "#partner-type-field"
+    SERVICES_DROPDOWN   = "#service-types-field"
+    SUBSCRIPTION_DROP   = "#subscription-tier-field"
+
+    # Dropdown options
+    TYPE_SERVICE_OPTION = "div[label='Service']"
+    # First available option in any open dropdown
+    FIRST_DROPDOWN_OPT  = ".ant-select-item-option:first-child"
+
+    # File upload - hidden input inside the upload div
+    UPLOAD_INPUT        = "#image-upload-button input[type='file']"
+
+    # Modal actions
+    SAVE_BUTTON         = "#save-button"
+    CANCEL_BUTTON       = "#cancel-button"
+
+    # Modal container - used to wait for close
+    MODAL_WRAPPER       = ".ant-modal-wrap"
+
+
+class EditMenuSelectors:
+    EDIT_BUTTON         = "#edit-button"
+    DELETE_BUTTON       = "#delete-button"
